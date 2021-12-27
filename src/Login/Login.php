617b2872d0d8caea9ebdf7342916703f679bdf37
bilal asghar <asgharbilal8@gmail.com>
2021-12-27 22:49:33 +0000

    added login form

--- src/Login/Login.php
+++ src/Login/Login.php
@@ -1,4 +1,4 @@
 <?php
-$page_content = "Login_Content.php";
-include("../MasterPages/master.php");
-?>
+$page_header = '';
+$page_content = 'Login_Content.php';
+include('../MasterPages/master.php');
--- src/Login/Login_Content.php
+++ src/Login/Login_Content.php
@@ -1,1 +1,29 @@
-<p>This page will contain the login</p>
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card">
+            <div class="card-header">
+                <h3><i class="fas fa-sign-in-alt"></i> Login</h3>
+            </div>
+            <div class="card-body">
+                <form action='' method=''>
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Select User</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class="form-group" style="margin-top: 10px;">
+                        <label for="username">Password</label>
+                        <input type="text" name="password" class="form-control" disabled value="****">
+                    </div>
+                    <div class="form-group" style="margin-top: 20px;">
+                        <button type="submit" class="btn btn-primary btn-block"><i class="fas fa-sign-in-alt"></i> Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
