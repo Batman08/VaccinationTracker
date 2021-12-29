35852d84dc1fd46f54d31b970367398839d5c481
bilal asghar <asgharbilal8@gmail.com>
2021-12-29 23:52:57 +0000

    created spGetVaccinationCentres sproc which will get a list of vaccination centres

--- database/MySQL Scripts/07 Stored Procedures.sql
+++ database/MySQL Scripts/07 Stored Procedures.sql
@@ -16,3 +16,13 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetMedicalPersonName`(IN Id int)
 BEGIN
 SELECT CONCAT(FirstName , ' ' , LastName) AS MedicalPersonName FROM MedicalPersons where MedicalPersonId = Id;
 END
+
+
+-- [spGetVaccinationCentres]
+-- This will get a list of vaccination centres after login
+--------------------------------------------------------------
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetVaccinationCentres`()
+BEGIN
+SELECT VaccinationCentreId, Name AS VaccinationCentreName, CONCAT(Address, ', ' , Postcode) AS VaccinationCentreAddress, Telephone AS VaccinationCentreTelephone FROM VaccinationCentres ORDER BY VaccinationCentreId;
+END
