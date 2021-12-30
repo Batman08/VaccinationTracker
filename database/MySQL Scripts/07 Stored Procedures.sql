ee3c19cf0ed19d3f3132d8a2c7ea59e9e07ee7bb
bilal asghar <asgharbilal8@gmail.com>
2021-12-30 17:53:54 +0000

    fixed errors when running

--- database/MySQL Scripts/07 Stored Procedures.sql
+++ database/MySQL Scripts/07 Stored Procedures.sql
@@ -1,28 +1,36 @@
+USE `Vaccinations`;
+
 -- [spGetUsernames]
 -- This will get the list if medical persons that can login
------------------------------------------------------------
+-- --------------------------------------------------------
 
+DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetUsernames`()
 BEGIN
     SELECT MedicalPersonId, CONCAT(FirstName , ' ' , LastName , ' (' , Profession , ')') AS Username FROM MedicalPersons ORDER BY FirstName, LastName;
-END
+END$$
+DELIMITER ;
 
 
 -- [spGetMedicalPersonName]
 -- This will get the selected medical persons name after login
---------------------------------------------------------------
+-- -----------------------------------------------------------
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetMedicalPersonName`(IN Id int)
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetMedicalPerson`(IN medicalPersonId int)
 BEGIN
-SELECT CONCAT(FirstName , ' ' , LastName) AS MedicalPersonName FROM MedicalPersons where MedicalPersonId = Id;
-END
+SELECT * FROM MedicalPersons where MedicalPersonId = medicalPersonId;
+END$$
+DELIMITER ;
 
 
 -- [spGetVaccinationCentres]
 -- This will get a list of vaccination centres after login
---------------------------------------------------------------
+-- -------------------------------------------------------
 
+DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `spGetVaccinationCentres`()
 BEGIN
 SELECT VaccinationCentreId, Name AS VaccinationCentreName, CONCAT(Address, ', ' , Postcode) AS VaccinationCentreAddress, Telephone AS VaccinationCentreTelephone FROM VaccinationCentres ORDER BY VaccinationCentreId;
-END
+END$$
+DELIMITER ;
