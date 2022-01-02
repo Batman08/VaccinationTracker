2a271c898bad3774b75acef80b3aead2289f0c0e
bilal asghar <asgharbilal8@gmail.com>
2022-01-02 13:58:19 +0000

    made vaccinate patient page

--- src/PatientVaccinations/VaccinatePatient_Content.php
+++ src/PatientVaccinations/VaccinatePatient_Content.php
@@ -1,26 +1,73 @@
 <?php
 session_start();
-$medicalPersonName = GetMedicalPersonName($_SESSION['MedicalPersonId']);
+$medicalPerson = GetMedicalPerson($_SESSION['medicalPersonId'])[0];
 $vaccinationCentres = GetVaccinationCentres();
+$vaccinationTypes = GetVaccinationTypes();
 ?>
 
-<html>
+<div class="row padBottom10">
+    <div class="col-sm-12">
+        <h3><?= $medicalPerson['FirstName'] . ' ' . $medicalPerson['LastName'] . ' - ' . $medicalPerson['Profession']; ?></h3>
+        <div class="text-muted"><?= $medicalPerson['Address'] . ', ' . $medicalPerson['Postcode'] . ', Tel:' . $medicalPerson['Telephone']; ?></div>
+    </div>
+</div>
 
-<body>
-    <?php foreach ($medicalPersonName as $name) { ?>
-        <label><?= 'Medical Person: ' . $name['MedicalPersonName']; ?></label>
-    <?php } ?>
+<div class="row padBottom10">
+    <div class="col-sm-3">
+        Choose Vaccination Centre:
+    </div>
+    <div class="col-sm-9">
+        <select name="ddlVaccinationCentre" class="form-select" aria-label="select vaccination centre">
+            <option selected disabled="disabled">-- Select Vaccination Centre --</option>
+                <?php foreach ($vaccinationCentres as $vaccinationCentre) { ?>
+            <option value="<?= $vaccinationCentre['VaccinationCentreId'] ?>"><?= $vaccinationCentre['VaccinationCentreName'] ?></option>
+        <?php } ?>
+        </select>
+    </div>
+</div>
 
-    </br>
-    </br>
+<hr />
 
-    <label for="txtVaccinationCentre">Choose Vaccination Centre:</label>
-    <select name="txtVaccinationCentre" class="form-select" aria-label="select user">
-        <option selected disabled="disabled">-- Select Vaccination Centre --
-            <?php foreach ($vaccinationCentres as $vaccinationCentre) { ?>
-        <option value="<?= $vaccinationCentre['VaccinationCentreId'] ?>"><?= $vaccinationCentre['VaccinationCentreName'] ?></option>
-    <?php } ?>
-    </select>
-</body>
+<form action="ViewNewPatient_content.php" method="post">
 
-</html>
+    <div class="form-group padBottom10">
+        <label for="vaccination_datetime">Date</label>
+        <input type="datetime-local" class="form-control" id="vaccination_datetime" name="vaccination_datetime">
+    </div>
+    <div class="form-group padBottom10">
+        <label for="patient_first_name">Patient First Name</label>
+        <input type="text" class="form-control" id="patient_first_name" name="patient_first_name" placeholder="Enter Patient First Name">
+    </div>
+    <div class="form-group padBottom10">
+        <label for="patient_last_name">Patient Last Name</label>
+        <input type="text" class="form-control" id="patient_last_name" name="patient_last_name" placeholder="Enter Patient Last Name">
+    </div>
+    <div class="form-group padBottom10">
+        <label for="patient_dob">Date of Birth</label>
+        <input type="date" class="form-control" id="patient_dob" name="patient_dob">
+    </div>
+    <div class="form-group padBottom10">
+        <label for="vaccination_type">Vaccination Type</label>
+        <select name="ddlVaccinationType" class="form-select" aria-label="select user">
+            <option selected disabled="disabled">-- Select Vaccination Type --</option>
+                <?php foreach ($vaccinationTypes as $vaccinationType) { ?>
+            <option value="<?= $vaccinationType['VaccinationTypeId'] ?>"><?= $vaccinationType['Name'] ?></option>
+        <?php } ?>
+        </select>
+    </div>
+    <div class="form-group padBottom10">
+        <label for="patient_address">Patient Address</label>
+        <input type="text" class="form-control" id="patient_address" name="patient_address" placeholder="Enter Patient Address">
+    </div>
+    <div class="form-group padBottom10">
+        <label for="patient_postcode">Patient Postcode</label>
+        <input type="text" class="form-control" id="patient_postcode" name="patient_postcode" placeholder="Enter Patient Postcode">
+    </div>
+    <div class="form-group padBottom10">
+        <label for="patient_phone_number">Patient Phone Number</label>
+        <input type="tel" class="form-control" id="patient_phone_number" name="patient_phone_number" placeholder="Enter Patient Phone Number">
+    </div>
+    <div class="form-group padBottom10">
+    <button type="submit" class="btn btn-primary mb-3">Add Patient</button>
+  </div>
+</form>
