18f3dee833bbf25f89002c60a43b3d893a14585c
bilal asghar <asgharbilal8@gmail.com>
2022-01-04 12:35:14 +0000

    changed card text and added button icon

--- src/AdminReports/AdminReports_Content.php
+++ src/AdminReports/AdminReports_Content.php
@@ -8,7 +8,7 @@
             <div class="card-body">
                 <h5 class="card-title">Special title treatment</h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary"><i class="fas fa-play"></i> Run Report</a>
             </div>
         </div>
     </div>
@@ -17,16 +17,16 @@
             <div class="card-body">
                 <h5 class="card-title">Special title treatment</h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary"><i class="fas fa-play"></i> Run Report</a>
             </div>
         </div>
     </div>
     <div class="col-sm">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Vaccinations by Centre</h5>
+                <p class="card-text">Displays the number of vaccinations carried out by each centre.</p>
+                <a href="#" class="btn btn-primary"><i class="fas fa-play"></i> Run Report</a>
             </div>
         </div>
     </div>
