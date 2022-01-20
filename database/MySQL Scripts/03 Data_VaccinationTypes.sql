5611e987797f461c1c381767cbe067d6373be6c4
bilal asghar <asgharbilal8@gmail.com>
2022-01-20 16:38:43 +0000

    fixed insert statements syntax for Data_VaccinationTypes and Data_MedicalPersons

--- database/MySQL Scripts/03 Data_VaccinationTypes.sql
+++ database/MySQL Scripts/03 Data_VaccinationTypes.sql
@@ -1,34 +1,34 @@
 USE `Vaccinations`;
 
-INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (1, N'Adenovirus Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (2, N'Anthrax Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (3, N'Cholera Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (4, N'COVID-19 Janssen Vaccine')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (5, N'COVID-19 Moderna Vaccine')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (6, N'COVID-19 Pfizer BioNTech Vaccine')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (7, N'COVID-19 AstraZeneca Vaccine')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (8, N'DTaP (Diphtheria, Tetanus, Pertussis) Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (9, N'Haemophilus Influenzae type b (Hib) Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (10, N'Hepatitis A Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (11, N'Hepatitis B Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (12, N'HPV (Human Papillomavirus) Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (13, N'Influenza (Flu) Vaccine (Inactivated or Recombinant) VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (14, N'Influenza (Flu) Vaccine (Live, Intranasal) VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (15, N'Japanese Encephalitis Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (16, N'Live Zoster Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (17, N'Meningococcal ACWY Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (18, N'Meningococcal B Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (19, N'MMR Vaccine (Measles, Mumps, and Rubella) VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (20, N'MMRV Vaccine (Measles, Mumps, Rubella, and Varicella) VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (21, N'Multi Pediatric Vaccines VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (22, N'Pneumococcal Conjugate Vaccine (PCV13) VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (23, N'Polio Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (24, N'PPSV23 VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (25, N'Rabies Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (26, N'Recombinant Zoster Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (27, N'Rotavirus Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (28, N'Td (Tetanus, Diphtheria) Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (29, N'Tdap (Tetanus, Diphtheria, Pertussis) Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (30, N'Typhoid VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (31, N'Varicella (Chickenpox) Vaccine VIS')
-;INSERT VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (32, N'Yellow Fever VIS')
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (1, N'Adenovirus Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (2, N'Anthrax Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (3, N'Cholera Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (4, N'COVID-19 Janssen Vaccine');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (5, N'COVID-19 Moderna Vaccine');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (6, N'COVID-19 Pfizer BioNTech Vaccine');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (7, N'COVID-19 AstraZeneca Vaccine');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (8, N'DTaP (Diphtheria, Tetanus, Pertussis) Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (9, N'Haemophilus Influenzae type b (Hib) Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (10, N'Hepatitis A Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (11, N'Hepatitis B Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (12, N'HPV (Human Papillomavirus) Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (13, N'Influenza (Flu) Vaccine (Inactivated or Recombinant) VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (14, N'Influenza (Flu) Vaccine (Live, Intranasal) VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (15, N'Japanese Encephalitis Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (16, N'Live Zoster Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (17, N'Meningococcal ACWY Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (18, N'Meningococcal B Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (19, N'MMR Vaccine (Measles, Mumps, and Rubella) VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (20, N'MMRV Vaccine (Measles, Mumps, Rubella, and Varicella) VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (21, N'Multi Pediatric Vaccines VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (22, N'Pneumococcal Conjugate Vaccine (PCV13) VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (23, N'Polio Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (24, N'PPSV23 VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (25, N'Rabies Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (26, N'Recombinant Zoster Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (27, N'Rotavirus Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (28, N'Td (Tetanus, Diphtheria) Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (29, N'Tdap (Tetanus, Diphtheria, Pertussis) Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (30, N'Typhoid VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (31, N'Varicella (Chickenpox) Vaccine VIS');
+INSERT INTO VaccinationTypes (`VaccinationTypeId`, `Name`) VALUES (32, N'Yellow Fever VIS');
