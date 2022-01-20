036efa38ff54379150467a21875ae89b372dcb75
bilal asghar <asgharbilal8@gmail.com>
2022-01-20 21:31:03 +0000

    fixed insert statements syntax

--- database/MySQL Scripts/05 Data_VaccinationCentres.sql
+++ database/MySQL Scripts/05 Data_VaccinationCentres.sql
@@ -1,7 +1,7 @@
 USE `Vaccinations`;
 
-INSERT VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (1, N'A R K Healthcare - Sparkhill', N'566-568 Stratford Road, Sparkhill, Birmingham', N'B11 4AN', N'0121 772 7381');
-INSERT VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (2, N'Lytham Road Pharmacy - Blackpool', N'South Shore Primary Care Centre, Lytham Road, Blackpool', N'FY4 1TJ', N'01253 403038');
-INSERT VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (3, N'Boots - Boots Colchester Lion Walk', N'5-6 Lion Walk Colchester, Essex', N'CO1 1LX', N'01206 577303');
-INSERT VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (4, N'Rowlands Pharmacy - Shipley', N'Windhill Green Med Centre, 2 Thackley Old Road, Shipley, West Yorkshire', N'BD18 1QB', N'01274 531214');
-INSERT VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (5, N'Avonmouth Pharmacy', N'205 Avonmouth Road, Avonmouth, Bristol', N'BS11 9EG', N'0117 982 3158');
+INSERT INTO VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (1, N'A R K Healthcare - Sparkhill', N'566-568 Stratford Road, Sparkhill, Birmingham', N'B11 4AN', N'0121 772 7381');
+INSERT INTO VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (2, N'Lytham Road Pharmacy - Blackpool', N'South Shore Primary Care Centre, Lytham Road, Blackpool', N'FY4 1TJ', N'01253 403038');
+INSERT INTO VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (3, N'Boots - Boots Colchester Lion Walk', N'5-6 Lion Walk Colchester, Essex', N'CO1 1LX', N'01206 577303');
+INSERT INTO VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (4, N'Rowlands Pharmacy - Shipley', N'Windhill Green Med Centre, 2 Thackley Old Road, Shipley, West Yorkshire', N'BD18 1QB', N'01274 531214');
+INSERT INTO VaccinationCentres (`VaccinationCentreId`, `Name`, `Address`, `Postcode`, `Telephone`) VALUES (5, N'Avonmouth Pharmacy', N'205 Avonmouth Road, Avonmouth, Bristol', N'BS11 9EG', N'0117 982 3158');
